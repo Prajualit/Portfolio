f1d974496f5a6da33efccd1c3ca5309ca68be2f8
Prajualit Tickoo <23bec072@nith.ac.in>
2025-05-17 19:15:10 +0530

    sidebar

--- src/common/components/sidebar/ProfileHeader.tsx
+++ src/common/components/sidebar/ProfileHeader.tsx
@@ -19,24 +19,24 @@ const ProfileHeader = ({ expandMenu, imageSize }: ProfileHeaderProps) => {
       )}
     >
       <Image
-        src='/images/aulianza-new.png'
-        alt='Ryan Aulia'
+        src='/images/Prajualit.png'
+        alt='Prajualit Tickoo'
         width={expandMenu ? 80 : imageSize}
         height={expandMenu ? 80 : imageSize}
         rounded='rounded-full'
-        className='rotate-3 dark:border-neutral-600 lg:hover:scale-105'
+        className=' bg-contain dark:border-neutral-600 lg:hover:scale-105'
       />
       <>
         <div className='mt-1 flex items-center gap-2 lg:mt-4'>
           <Link href='/' passHref>
             <h2 className='flex-grow  text-lg font-medium lg:text-xl'>
-              Ryan Aulia
+              Prajualit Tickoo
             </h2>
           </Link>
           <VerifiedIcon size={18} className='text-blue-400' />
         </div>
         <div className='hidden text-[15px] text-neutral-600 transition-all duration-300 hover:text-neutral-700 dark:text-neutral-500 dark:hover:text-neutral-400 lg:flex'>
-          @aulianza
+          @prajualit
         </div>
       </>
     </div>
