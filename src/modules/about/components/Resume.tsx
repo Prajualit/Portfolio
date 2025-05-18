4043a52cdba59d4a274ec03a0f522ac175251ced
Prajualit Tickoo <23bec072@nith.ac.in>
2025-05-19 02:21:30 +0530

    resume section

--- src/modules/about/components/Resume.tsx
+++ src/modules/about/components/Resume.tsx
@@ -4,7 +4,7 @@ import { LuDownload as DownloadIcon } from 'react-icons/lu';
 import GoogleDocsEmbed from '@/common/components/elements/GoogleDocsEmbed';
 
 const Resume = () => {
-  const RESUME_URL = 'https://bit.ly/cv-aulianza';
+  const RESUME_URL = 'https://docs.google.com/document/d/1emdLRJSPe3lBe42D9Szb9ectYJBX6tZNy9zglll1_hc/export?format=pdf';
 
   return (
     <div className='space-y-5'>
@@ -19,7 +19,7 @@ const Resume = () => {
         <span>Download Resume & Template</span>
       </Link>
 
-      <GoogleDocsEmbed src='https://docs.google.com/document/d/1Ug6V5cI9D3hBPD8t5qrsvC3Zr2H1-EKlu4I_q7fcigw/edit' />
+      <GoogleDocsEmbed src='https://docs.google.com/document/d/1emdLRJSPe3lBe42D9Szb9ectYJBX6tZNy9zglll1_hc/edit?tab=t.0' />
     </div>
   );
 };
