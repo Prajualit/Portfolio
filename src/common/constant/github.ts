73f86185d9b94e68f594a7438fcf56aef4056711
Prajualit Tickoo <23bec072@nith.ac.in>
2025-05-19 02:23:20 +0530

    dashboard

--- src/common/constant/github.ts
+++ src/common/constant/github.ts
@@ -1,13 +1,13 @@
 export const GITHUB_ACCOUNTS = [
   // {
-  //   username: 'ryan-sirka',
+  //   username: 'ryan-sirka',hioijo
   //   token: process.env.GITHUB_READ_USER_TOKEN_WORK,
   //   endpoint: '/api/github?type=work',
   //   type: 'work',
   //   is_active: true,
   // },
   {
-    username: 'aulianza',
+    username: 'prajualit',
     token: process.env.GITHUB_READ_USER_TOKEN_PERSONAL,
     endpoint: '/api/github?type=personal',
     type: 'personal',
--- src/common/libs/firebase.ts
+++ src/common/libs/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import { getApp,getApps, initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
@@ -6,12 +6,12 @@ const firebaseConfig = {
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL as string,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
-  messagingSenderId: process.env
-    .NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID as string,
 };
 
-const firebase = initializeApp(firebaseConfig);
+// Check if Firebase app is already initialized
+const firebase = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 export { firebase };
